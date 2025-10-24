b68a0e65c14e4d071c89fb7df978546b9a7f2e9b
Christoph Massmann <cm@vianetz.com>
2025-10-24 18:38:04 +0200

    changed event name to singular

--- src/js/utilities.ts
+++ src/js/utilities.ts
@@ -1,2 +1,2 @@
 export const emit = (el: Element, id: string, detail?: any, cancelable = false) =>
-  el.dispatchEvent(new CustomEvent(`via-animated-headlines:${id}`, { bubbles: true, cancelable, detail }));
+  el.dispatchEvent(new CustomEvent(`via-animated-headline:${id}`, { bubbles: true, cancelable, detail }));
